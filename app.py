2c4ccca7143bcd3aafd5351cb65e5be53c942485
KSTARK007 <kiranhombal98@gmail.com>
2019-02-12 20:33:21 +0530

    10 apis done

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import *
 import hashlib
 from pymongo import *
 import string 
+import datetime
+import re
 
 app = Flask(__name__)
 
@@ -9,7 +11,7 @@ app = Flask(__name__)
 @app.errorhandler(404)
 def page_not_found(e):
     # note that we set the 404 status explicitly
-    return "bal", 404
+    return "bla",404
 
 @app.route('/')
 def index():
@@ -18,6 +20,7 @@ def index():
 client = MongoClient(port=27017)
 db=client.cc_assignment.users
 cat = client.cc_assignment.categories
+act = client.cc_assignment.act
 
 def getNextSequence(collection,name):
 	collection.update_one( { '_id': name },{ '$inc': {'seq': 1}});
@@ -67,9 +70,13 @@ def categorieAdd():
 #api 4
 @app.route('/api/v1/categories', methods=['POST'])
 def categorieList():
-	j = request.get_json()
-	for k , v in j.items()	:
-		result=cat.insert_one({'catId': getNextSequence(client.cc_assignment.orgid_counter,"catId"), 'catName': k, 'size' : v })
+	j = re.search("[A-Za-z0-9 _]+",(request.get_data().decode('utf-8')))
+	if(j is None):
+		return jsonify({'code':400})
+	j = j.group(0)
+	if(cat.count_documents({"catName":j})>0):
+		return jsonify({'code':404})
+	result=cat.insert_one({'catId': getNextSequence(client.cc_assignment.orgid_counter,"catId"), 'catName':j , 'size' : 0 })
 	return jsonify({'code':200})
 
 
@@ -80,8 +87,103 @@ def catdelete(categories):
 		cat.delete_one({"catName":categories})
 		return jsonify({'code':200})
 	else:
-		abort(404)
 		return jsonify({'code':404})
+
+#api 6
+@app.route('/api/v1/categories/<categoryName>/acts', methods=['GET'])
+def catactsizeprint(categoryName):
+	if(not cat.count_documents({"catName":categoryName})>0):
+		return jsonify({"code": 400})
+	if(len(request.args.get())>0):
+		return jsonify({'code':404})
+		print(request.args.get())
+	d = dict()
+	j = cat.find_one({"catName" : categoryName})
+	if(j['size'] < 100):
+		q = dict()
+		i = 0
+		if(act.count_documents({"catName":categoryName}) == 0):
+			return jsonify({'code':404})
+		v = act.find({"catName" : categoryName},{"_id":0})
+		for x in v:
+			q[i] = x
+			i =  i+1
+		return jsonify(q),200
+	else:
+		return jsonify({"code":413})
+
+#api 7
+@app.route('/api/v1/categories/<categories>/acts/size', methods=['GET'])
+def catactsize(categories):
+	if(not cat.count_documents({"catName":categories})>0):
+		return jsonify({"code": 400})
+	else:
+		j = cat.find({"catName" : categories})
+	   		for x in j:
+			return jsonify(x['size'])
+
+
+
+#api 9
+@app.route('/api/v1/acts/upvote', methods=['POST'])
+def upvote():
+	j = request.get_json()
+	if(not act.count_documents({"actId":j['actId']})>0):
+		return jsonify({"code": 400})
+	else:
+		act.update_one( { 'actId': j['actId'] },{ '$inc': {'upvote': 1}})
+		return jsonify({"code": 200})
+
+#api 10
+@app.route('/api/v1/acts/<actId>', methods=['DELETE'])
+def actDelete(actId):
+	if(not act.count_documents({"actId":int(actId)})>0):
+		return jsonify({"code": 400})
+	else:
+		act.delete_one({"actId":int(actId)})
+		cat.update_one({ 'catName':x['catName'] for x in act.find({"actId":int(actId)}) },{ '$inc': {'size': -1}})
+		return jsonify({'code':200})
+		
+
+def validateDateTime(date_text):
+    try:
+        datetime.datetime.strptime(date_text, '%d-%m-%Y:%S-%M-%H')
+        return True
+    except ValueError:
+        return False
+def validateBase64(data_text):
+	if(re.search("[A-Za-z0-9+/=]", data_text) and len(data_text)%4==0):
+		return True
+	else:
+		return False
+#api 11
+@app.route('/api/v1/acts', methods=['POST'])
+def actUpload():
+	j = request.get_json()
+	#to validate unique ID
+	if(act.count_documents({"actId":j['actId']})>0):
+		return jsonify({"code":405})
+	#to validate timestamp
+	if not validateDateTime(j['timestamp']):
+		return jsonify({"code":406})
+	#to validate user exists
+	if(not db.count_documents({"name":j['username']})>0):
+		return jsonify({"code":407})
+	#to validate Base64 code 
+	if(not validateBase64(j['imgB64'])):
+		return jsonify({"code":408})
+	#to validate upvote
+	if("upvote" in j):
+		return jsonify({"code":409})
+	#to validate that cat exists
+	if(not cat.count_documents({"catName":j['categoryName']})>0):
+		return jsonify({"code":410})
+
+	result=act.insert_one({'actId':j['actId'] , 'username': j['username'], 'timestamp' : j['timestamp'], 'caption':j['caption'], 'catName':j['categoryName'], 'img':j['imgB64'], 'upvote':0 })
+	cat.update_one({ 'catName':j['categoryName'] },{ '$inc': {'size': 1}})
+	return jsonify({'code':200})
+
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=80)
