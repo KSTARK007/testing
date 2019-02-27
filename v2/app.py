0619a947830d55fc6755b532a37f2affc8a0b45a
KSTARK007 <kiranhombal98@gmail.com>
2019-02-28 03:57:12 +0530

    version 2

--- v2/app.py
+++ v2/app.py
@@ -42,25 +42,27 @@ def process():
 	j = request.get_json()
 	name = j['name']
 	password = j['password']
-	if( len(password) != 40 or not all(c in string.hexdigits for c in password) ):
-		return jsonify({'code' : 600})
+	if( len(password) != 40 or not all(c in string.hexdigits for c in password)):
+		return jsonify({'code' : 600,"text":"not in sha1"}),400
 
 	if name and password and password != "da39a3ee5e6b4b0d3255bfef95601890afd80709":
 		if(db.count_documents({"name":name})>0):
-			return jsonify({'code' : 405})
-
+			print("username already exist")
+			return jsonify({'code' : 405,"text":"username already exist"}),400
 		result=db.insert_one({'userId': getNextSequence(client.cc_assignment.orgid_counter,"userId"), 'name': name, 'password' : password })
-		return jsonify({'code' : 201})
-	return jsonify({'code' : 400})
+		return jsonify({'code' : 201}),200
+	print("empty fields")
+	return jsonify({'code' : 400,"text":"empty fields"}),400
 
 #api 2
 @app.route('/api/v1/users/<username>', methods=['DELETE'])
 def userdelete(username):
 	if(db.count_documents({"name":username})>0):
 		db.delete_one({"name":username})
-		return jsonify({'code':200})
+		return jsonify({'code':200}),200
 	else:
-		return jsonify({'code':404})
+		print("api 2 user does not exist")
+		return jsonify({'code':404,"text":"user does not exist" }),400
 
 #api 3
 @app.route('/api/v1/categories', methods=['GET'])
@@ -69,66 +71,75 @@ def categorieAdd():
 	d = dict()
 	for x in j:
 		d[x['catName']]=x['size']
-	return jsonify(d)
+	return jsonify(d),200
 
 #api 4
 @app.route('/api/v1/categories', methods=['POST'])
 def categorieList():
 	j = re.search("[A-Za-z0-9 _]+",(request.get_data().decode('utf-8')))
 	if(j is None):
-		return jsonify({'code':400})
+		print("empty input")
+		return jsonify({'code':400}),400
 	j = j.group(0)
 	if(cat.count_documents({"catName":j})>0):
-		return jsonify({'code':404})
+		print("categoryName already exist")
+		return jsonify({'code':404}),400
 	result=cat.insert_one({'catId': getNextSequence(client.cc_assignment.orgid_counter,"catId"), 'catName':j , 'size' : 0 })
-	return jsonify({'code':200})
+	return jsonify({'code':200}),200
 
 #api 5
 @app.route('/api/v1/categories/<categories>', methods=['DELETE'])
 def catdelete(categories):
 	if(cat.count_documents({"catName":categories})>0):
 		cat.delete_one({"catName":categories})
-		return jsonify({'code':200})
+		return jsonify({'code':200}),200
 	else:
-		return jsonify({'code':404})
+		print("categoryName does not exists")
+		return jsonify({'code':404}),400
 
 #api 6 and 8
 @app.route('/api/v1/categories/<categoryName>/acts', methods=['GET'])
 def catactsizeprint(categoryName):
 	start = request.args.get("start")
 	end = request.args.get("end")
+	if(not cat.count_documents({"catName":categoryName})>0):
+		print("categoryName does not exists")
+		return jsonify({"code": 400}),400
 	if(start is None and end is None):
-		if(not cat.count_documents({"catName":categoryName})>0):
-			return jsonify({"code": 400})
 		d = dict()
 		j = cat.find_one({"catName" : categoryName})
 		if(j['size'] < 100):
 			l = list()
 			if(act.count_documents({"catName":categoryName}) == 0):
-				return jsonify({'code':404})
-			v = act.find({"catName" : categoryName},{"_id":0})
+				print("empty category")
+				return jsonify({'code':404}),200
+			v = act.find({"catName" : categoryName},{"_id":0,"catName":0})
 			for x in v:
 				l.append(x)
 			return jsonify(l),200
 		else:
-			return jsonify({"code":413})
+			print("more than 100 asked api6")
+			return jsonify({"code":413}),400
 	if(start is None or end is None):
-		return jsonify({"code":1400})
+		print("start or end missing")
+		return jsonify({"code":1400}),400
 	else :
 		start = int(start)
 		end = int(end)
 		if(start > end or (start<0 or end <0)):
-			return jsonify({"code":1600})
+			print("start and end values and not correct")
+			return jsonify({"code":1600}),400
 		else :
-			diff = end-start
+			diff = end-start + 1
 			k = 1
 			ll = list()
 			val = act.count_documents({"catName":categoryName})
 			if(val < diff or diff >100):
+				print("more values than given or more than 100 values")
 				return jsonify({"code" : 1400,"text":"more than 100 values "}),400
 			if(val == 0):
-				return jsonify({'code':1404})
-			v = act.find({"catName" : categoryName},{"_id":0}).sort([("actId",-1)])
+				return jsonify({'code':1404}),400
+			v = act.find({"catName" : categoryName},{"_id":0}).sort([("timestamp",-1)])
 			for x in v:
 				if(k <= diff):
 					ll.append(x)
@@ -139,7 +150,7 @@ def catactsizeprint(categoryName):
 @app.route('/api/v1/categories/<categories>/acts/size', methods=['GET'])
 def catactsize(categories):
 	if(not cat.count_documents({"catName":categories})>0):
-		return jsonify({"code": 400})
+		return jsonify({"code": 400}),400
 	else:
 		j = cat.find({"catName" : categories})
 		for x in j:
@@ -150,7 +161,7 @@ def catactsize(categories):
 def upvote():
 	j = request.get_json()
 	if(not act.count_documents({"actId":j['actId']})>0):
-		return jsonify({"code": 400})
+		return jsonify({"code": 400}),400
 	else:
 		act.update_one( { 'actId': j['actId'] },{ '$inc': {'upvote': 1}})
 		return jsonify({"code": 200})
@@ -188,24 +199,24 @@ def actUpload():
 	j = request.get_json()
 	#to validate unique ID
 	if(act.count_documents({"actId":j['actId']})>0):
-		return jsonify({"code":405})
+		return jsonify({"code":405}),400
 	#to validate timestamp
 	if not validateDateTime(j['timestamp']):
-		return jsonify({"code":406})
+		return jsonify({"code":406}),400
 	#to validate user exists
 	if(not db.count_documents({"name":j['username']})>0):
-		return jsonify({"code":407})
+		return jsonify({"code":407}),400
 	#to validate Base64 code
-	if(not validateBase64(j['img'])):
-		return jsonify({"code":408})
+	if(not validateBase64(j['imgB64'])):
+		return jsonify({"code":408}),400
 	#to validate upvote
 	if("upvote" in j):
-		return jsonify({"code":409})
+		return jsonify({"code":409}),400
 	#to validate that cat exists
 	if(not cat.count_documents({"catName":j['categoryName']})>0):
-		return jsonify({"code":410})
+		return jsonify({"code":410}),400
 
-	result=act.insert_one({'actId':j['actId'] , 'username': j['username'], 'timestamp' : j['timestamp'], 'caption':j['caption'], 'catName':j['categoryName'], 'img':j['img'], 'upvote':0 })
+	result=act.insert_one({'actId':j['actId'] , 'username': j['username'], 'timestamp' : j['timestamp'], 'caption':j['caption'], 'catName':j['categoryName'], 'imgB64':j['imgB64'], 'upvote':0 })
 	cat.update_one({ 'catName':j['categoryName'] },{ '$inc': {'size': 1}})
 	client.cc_assignment.orgid_counter.update_one( {'_id':"actId"},{'$inc': {'seq': 1}})
 	return jsonify({'code':200})
@@ -222,7 +233,7 @@ def actid():
 def downvote():
 	j = request.get_json()
 	if(not act.count_documents({"actId":j['actId']})>0):
-		return jsonify({"code": 400})
+		return jsonify({"code": 400}),400
 	else:
 		act.update_one( { 'actId': j['actId'] },{ '$inc': {'upvote': -1}})
 		return jsonify({"code": 200})
@@ -234,11 +245,11 @@ def processes():
 	name = j['name']
 	password = j['password']
 	if( len(password) != 40 or not all(c in string.hexdigits for c in password) ):
-		return jsonify({'code' : 600 ,"text" :"Sha1 error"})
+		return jsonify({'code' : 600 ,"text" :"Sha1 error"}),200
 
 	if name and password and password != "da39a3ee5e6b4b0d3255bfef95601890afd80709":
 		if(db.count_documents({"name":name})<=0):
-			return jsonify({'code' : 405 ,"text" :"login fail"})
+			return jsonify({'code' : 405 ,"text" :"login fail"}),400
 
 		v = db.find_one({'name': name},{"_id":0})
 		return jsonify({'code' : 201,"text" :"Successfull login","userId":v["userId"]})
